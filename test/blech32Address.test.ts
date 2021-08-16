894a073404ff1ddee40f2f9a8cce114328c4ba1f
louisinger <louis@vulpem.com>
2021-08-16 14:56:19 +0200

    add tests cases for blech32 and blech32m from elements codebase

--- test/blech32Address.test.ts
+++ test/blech32Address.test.ts
@@ -1,12 +1,12 @@
 import { Blech32Address } from "../src";
 import {
-  validFixtures,
-  invalidFixtures
+  invalidFixtures,
+  validFixtures
 } from "./fixtures/blech32Address.fixture";
 import { strictEqual, throws } from "assert";
 
 describe("Blech32 Address", () => {
-  it("should compute blech32 addresses", () => {
+  it("should compute blech32 addresses from witness program, a blinding public key and a prefix", () => {
     for (const f of validFixtures) {
       const addr = Blech32Address.from(f.witness, f.blindingPublicKey, f.prefix)
         .address;
@@ -17,8 +17,8 @@ describe("Blech32 Address", () => {
   });
 
   it("should throw an error if we try to decode an invalid blech32 address", () => {
-    for (const { address } of invalidFixtures) {
-      throws(() => Blech32Address.fromString(address));
+    for (const str of invalidFixtures) {
+      throws(() => Blech32Address.fromString(str));
     }
   });
 });
--- test/fixtures/blech32Address.fixture.ts
+++ test/fixtures/blech32Address.fixture.ts
@@ -34,16 +34,8 @@ export const validFixtures = [
 ];
 
 export const invalidFixtures = [
-  {
-    address: "el1qqw3e3mk4ng3ks43mh54udznuekaadh9lgwef3mwgzkmdw" // invalid CT address
-  },
-  {
-    address: "ert1q2z45rh444qmeand48lq0wp3jatxs2nzh492ds9s5yscv2pplxwesajz7q3" // P2WSH (unconfidential)
-  },
-  {
-    address: "ex1qlg343tpldc4wvjxn3jdq2qs35r8j5yd5vqrmu3" // P2SH (unconfidential)
-  },
-  {
-    address: "" // empty string
-  }
+  "el1qqw3e3mk4ng3ks43mh54udznuekaadh9lgwef3mwgzkmdw", // invalid CT address
+  "ert1q2z45rh444qmeand48lq0wp3jatxs2nzh492ds9s5yscv2pplxwesajz7q3", // P2WSH (unconfidential)
+  "ex1qlg343tpldc4wvjxn3jdq2qs35r8j5yd5vqrmu3", // P2SH (unconfidential)
+  "",
 ];
